6cd2dc95b6866ffe7afe1d4970aef48ea9ab404c
cristian0123456 <79230856+cristian0123456@users.noreply.github.com>
2022-11-11 10:31:36 -0600

    Add files via upload

---
+++ Quintanilla CamposExamen DAW/js/javascripE.js
@@ -0,0 +1,21 @@
+
+function colores() {
+    var element = document.getElementById("id1");
+    element.style.backgroundColor='#900';
+    }
+
+    
+    function colores2() {
+        var element = document.getElementById("id2");
+        element.style.backgroundColor='#250';
+        }
+
+        function colores3() {
+            var element = document.getElementById("id3");
+            element.style.backgroundColor='#852';
+            }
+
+            function colores4() {
+                var element = document.getElementById("id4");
+                element.style.backgroundColor='#777';
+                }
